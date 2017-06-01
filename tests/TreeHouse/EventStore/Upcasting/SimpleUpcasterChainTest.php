97b958483f0ae6211112dff77a9e339d6316542c
Jeroen Fiege <fieg@users.noreply.github.com>
2017-06-01 13:35:54 +0200

    Merge 1.2 into 2.0 (#10)
    * Added support for upcasting to multiple events
    
    * Fix upcaster chain loop
    
    * Split tests
    
    * One array_push to rule them all
    
    * Trigger deprecation warning
    
    * updated change log
    
    * Silence trigger_error and improve test
    
    * Update CHANGELOG
    
    * deprecate MutableEventStoreInterface

---
+++ tests/TreeHouse/EventStore/Upcasting/SimpleUpcasterChainTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace TreeHouse\EventStore\Upcasting;
+
+use TreeHouse\EventStore\SerializedEvent;
+use TreeHouse\EventStore\Tests\DummyUpcaster;
+
+class SimpleUpcasterChainTest extends \PHPUnit_Framework_TestCase
+{
+    const UUID = '04928cbb-7062-4f4a-916f-105b43c54606';
+    /**
+     * @test
+     */
+    public function it_chains_upcasters()
+    {
+        $event = [new SerializedEvent(self::UUID, 'Foo', ['key' => 'value'], 2, 1, new \DateTime('2017-05-29 19:00:00'))];
+
+        $multipleEvents = [
+            new SerializedEvent(self::UUID, 'Foo', [], 3, 1, new \DateTime('2017-05-29 19:00:00')),
+            new SerializedEvent(self::UUID, 'Spliced', ['key' => 'value'], 1, 1,new \DateTime('2017-05-29 19:00:00')),
+        ];
+
+        $upcasterChain = new SimpleUpcasterChain();
+        $upcasterChain->registerUpcaster(
+            new DummyUpcaster(function (SerializedEvent $e) {
+                return true;
+            }, $event)
+        );
+        $upcasterChain->registerUpcaster(new DummyUpcaster(function (SerializedEvent $e) {
+            return $e->getName() === 'Foo' && $e->getPayloadVersion() === 2;
+        }, $multipleEvents));
+
+        $events = $upcasterChain->upcast(
+            $this->prophesize(SerializedEvent::class)->reveal(),
+            $this->prophesize(UpcastingContext::class)->reveal()
+        );
+
+        $this->assertInternalType('array', $events);
+        $this->assertContainsOnlyInstancesOf(SerializedEvent::class, $events);
+
+        $this->assertEquals([], $events[0]->getPayload());
+        $this->assertEquals(3, $events[0]->getPayloadVersion());
+
+        $this->assertEquals('Spliced', $events[1]->getName());
+        $this->assertEquals(['key' => 'value'], $events[1]->getPayload());
+        $this->assertEquals(1, $events[1]->getPayloadVersion());
+    }
+
+    /**
+     * @test
+     */
+    public function it_supports_non_collection_return_from_upcaster()
+    {
+        $event = new SerializedEvent(self::UUID, 'Foo', ['key' => 'value'], 2, 1, new \DateTime('2017-05-29 19:00:00'));
+        $event2 = new SerializedEvent(self::UUID, 'Foo', [], 3, 1, new \DateTime('2017-05-29 19:00:00'));
+
+        $upcasterChain = new SimpleUpcasterChain();
+        $upcasterChain->registerUpcaster(
+            new DummyUpcaster(function (SerializedEvent $e) {
+                return true;
+            }, $event)
+        );
+        $upcasterChain->registerUpcaster(new DummyUpcaster(function (SerializedEvent $e) {
+            return $e->getName() === 'Foo' && $e->getPayloadVersion() === 2;
+        }, $event2));
+
+        $upcastedEvent = $upcasterChain->upcast(
+            $this->prophesize(SerializedEvent::class)->reveal(),
+            $this->prophesize(UpcastingContext::class)->reveal()
+        );
+
+        $this->assertEquals([$event2], $upcastedEvent);
+    }
+}
